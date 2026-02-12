5b88e7a2744f4d3611269c8ebb2e7080e0c2f274
farelltimo <farelltimothy@gmail.com>
2026-02-13 00:03:37 +0700

    week01: (tugas 2) implement null safety for user note

--- src/OOP_108839_FarellTimothy/week01/GameStore.kt
+++ src/OOP_108839_FarellTimothy/week01/GameStore.kt
@@ -3,18 +3,24 @@ package oop_108839_FarellTimothy.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
-    // Lanjut ke langkah berikutnya...
+    val userNote: String? = null
 
     val discountPercent = calculateDiscount(price)
     val finalPrice = price - (price * discountPercent / 100)
 
-    printReceipt(title = gameTitle, finalPrice = finalPrice)
-
-    // Di luar main() tambahkan:
-    fun printReceipt(title: String, finalPrice: Int) {
-        println("=== STRUK PEMBELIAN STEAMKW ===")
-        println("Game: $title")
-        println("Harga Akhir: Rp $finalPrice")
+    // Pemanggilan fungsi dengan Named Arguments
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice,
+        note = userNote
+    )
 }
+fun calculateDiscount(price: Int) = if (price > 500000) 20
+else 10
 
-fun calculateDiscount(price: Int) = if (price > 500000) 20 else 10
+fun printReceipt(title: String, finalPrice: Int, note: String?) {
+    println("=== STRUK PEMBELIAN STEAMKW ===")
+    println("Game: $title")
+    println("Harga Akhir: Rp $finalPrice")
+    // Menggunakan Elvis Operator (?:) sesuai instruksi modul
+    println("Catatan: ${note ?: "Tidak ada catatan"}")

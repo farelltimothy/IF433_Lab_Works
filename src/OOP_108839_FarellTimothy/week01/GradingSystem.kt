c28c5dd1511f99c6fccd0ec0e96a1147b5a77764
farelltimo <farelltimothy@gmail.com>
2026-02-12 23:46:25 +0700

    week01: initial setup and procedural code

---
+++ src/OOP_108839_FarellTimothy/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package OOP_108839_FarellTimothy.week01
+
+fun main() {
+    var name: String = "Farell"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + " Nilai: " + score)
+}

98a7397d1e137986ed4814d5ac7316c72c03e787
farelltimo <farelltimothy@gmail.com>
2026-02-12 23:59:03 +0700

    week01: (tugas 2) implement receipt printing

--- src/OOP_108839_FarellTimothy/week01/GameStore.kt
+++ src/OOP_108839_FarellTimothy/week01/GameStore.kt
@@ -4,6 +4,17 @@ fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
     // Lanjut ke langkah berikutnya...
+
+    val discountPercent = calculateDiscount(price)
+    val finalPrice = price - (price * discountPercent / 100)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
+
+    // Di luar main() tambahkan:
+    fun printReceipt(title: String, finalPrice: Int) {
+        println("=== STRUK PEMBELIAN STEAMKW ===")
+        println("Game: $title")
+        println("Harga Akhir: Rp $finalPrice")
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) 20 else 10

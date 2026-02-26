c1ec1a934a8fe704c1de79e2b0a7930ca591c3a6
farelltimo <farelltimothy@gmail.com>
2026-02-26 19:35:27 +0700

    week03: fix setter using backing field

--- src/OOP_108839_FarellTimothy/week3/employee.kt
+++ src/OOP_108839_FarellTimothy/week3/employee.kt
@@ -3,9 +3,11 @@ package OOP_108839_FarellTimothy.week3
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
-            // Memanggil setter di dalam setter itu sendiri (Recursive)
-            this.salary = value
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
--- src/OOP_108839_FarellTimothy/week3/main.kt
+++ src/OOP_108839_FarellTimothy/week3/main.kt
@@ -2,8 +2,12 @@ package OOP_108839_FarellTimothy.week3
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
 
-    // Baris di bawah ini akan memicu ledakan program (StackOverflowError)
+    println("--- Test Validasi Gaji ---")
+
     e.salary = 5000
+    println("Gaji ${e.name}: ${e.salary}")
+
+    e.salary = -1000
+    println("Gaji ${e.name} setelah input negatif: ${e.salary}")
 }

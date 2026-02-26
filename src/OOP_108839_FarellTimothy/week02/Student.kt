e67f8bbce1e15149c4831acbede9dedbe84e1eaf
farelltimo <farelltimothy@gmail.com>
2026-02-26 09:07:37 +0700

    week02: add secondary constructor

--- src/OOP_108839_FarellTimothy/week02/Student.kt
+++ src/OOP_108839_FarellTimothy/week02/Student.kt
@@ -5,6 +5,21 @@ class Student (
     val nim: String,
     var major: String
     ){
+
+
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
@@ -14,3 +29,4 @@ class Student (
         }
     }
 }
+

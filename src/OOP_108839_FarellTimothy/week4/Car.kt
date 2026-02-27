7c1cc435b878881f38a8ec25f5976e59194357ac
farelltimo <farelltimothy@gmail.com>
2026-02-27 10:56:31 +0700

    week04: create Car class extending Vehicle

---
+++ src/OOP_108839_FarellTimothy/week4/Car.kt
@@ -0,0 +1,8 @@
+package OOP_108839_FarellTimothy.week04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}

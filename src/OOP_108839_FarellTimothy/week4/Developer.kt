22d8725062a7fb72fb68c14b5225b94dd9c369be
farelltimo <farelltimothy@gmail.com>
2026-02-27 11:12:00 +0700

    week04: (task 2) create Developer class

---
+++ src/OOP_108839_FarellTimothy/week4/Developer.kt
@@ -0,0 +1,8 @@
+package OOP_108839_FarellTimothy.week04
+
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}

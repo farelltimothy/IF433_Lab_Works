3f85631c9ae2400109ea2a79f5742d7ffa40decd
farelltimo <farelltimothy@gmail.com>
2026-03-13 12:09:20 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/OOP_108839_FarellTimothy/week06/Gadgetinterfaces.kt
@@ -0,0 +1,13 @@
+package oop_108839_FarellTimothy.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}

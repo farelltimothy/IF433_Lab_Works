bac0840e1b26d5e77c067fd0b0323c917fe83988
farelltimo <farelltimothy@gmail.com>
2026-03-13 11:40:52 +0700

    week05: (task 2) create CreditCard class

---
+++ src/OOP_108839_FarellTimothy/week06/CreditCard.kt
@@ -0,0 +1,14 @@
+package oop_108839_FarellTimothy.week06
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("[$accountName] Pembayaran via Credit Card sebesar $amount berhasil. Total tagihan: $usedAmount")
+        } else {
+            println("[$accountName] Transaksi ditolak. Melebihi limit kartu kredit.")
+        }
+    }
+}

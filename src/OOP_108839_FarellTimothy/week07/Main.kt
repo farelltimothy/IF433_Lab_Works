0882f7d07533eb35556c3706ff0ba3f49ffbff32
farelltimo <farelltimothy@gmail.com>
2026-03-27 11:39:21 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/OOP_108839_FarellTimothy/week07/Main.kt
+++ src/OOP_108839_FarellTimothy/week07/Main.kt
@@ -30,4 +30,13 @@ fun main() {
 
     val (userName, userAge) = data1
     println("Destructured: $userName berumur $userAge")
+
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    // ERROR: 'when' expression must be exhaustive (kurang state Loading)
+    val uiMessage = when(response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
 }

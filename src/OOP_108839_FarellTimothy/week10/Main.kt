906cbec2f8b422ed2c52f6f40a8fab649269f37f
farelltimo <farelltimothy@gmail.com>
2026-05-08 11:52:08 +0700

    week10: implement PairBox in main function

--- src/OOP_108839_FarellTimothy/week10/Main.kt
+++ src/OOP_108839_FarellTimothy/week10/Main.kt
@@ -12,4 +12,12 @@ fun main() {
     // Mencetak nilai tanpa perlu casting manual
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
+
+    println("\n=== TEST MULTIPLE PARAMETERS ===")
+
+    // Instansiasi PairBox dengan tipe String (Key) dan Int (Value)
+    val itemPrice = PairBox("Bitcoin", 65000)
+
+    // Mencetak properti key dan value dari PairBox
+    println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
 }

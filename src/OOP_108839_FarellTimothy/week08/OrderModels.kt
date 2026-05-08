0ee3df26032400ec3a3c7070a56917a246f3cbcd
farelltimo <farelltimothy@gmail.com>
2026-05-08 09:40:23 +0700

    week08: create nested classes for Order

---
+++ src/OOP_108839_FarellTimothy/week08/OrderModels.kt
@@ -0,0 +1,9 @@
+package OOP_108839_FarellTimothy.week08
+
+class City(val name: String)
+
+class Address(val city: City?)
+
+class DeliveryDetails(val address: Address?)
+
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)

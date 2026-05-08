0c7de940175cdd5f3d979802b20465d1855279a8
farelltimo <farelltimothy@gmail.com>
2026-05-08 10:03:43 +0700

    week08: test safe casting in main

--- src/OOP_108839_FarellTimothy/week08/Main.kt
+++ src/OOP_108839_FarellTimothy/week08/Main.kt
@@ -30,10 +30,13 @@ fun main() {
     for (item in mixedData) {
         val text = item as? String
 
-        // Hanya cetak jika cast sukses (text tidak null)
         text?.let {
             println("Ditemukan teks: ${it.uppercase()}")
         }
     }
+
+    val someObject: Any = 100
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast + fallback: $safeString")
 }
 

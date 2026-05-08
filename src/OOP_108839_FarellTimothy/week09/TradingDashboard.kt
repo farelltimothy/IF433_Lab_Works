c88072c06f70e44418fa61820276cbc16a32fd51
farelltimo <farelltimothy@gmail.com>
2026-05-08 11:34:38 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/OOP_108839_FarellTimothy/week09/TradingDashboard.kt
+++ src/OOP_108839_FarellTimothy/week09/TradingDashboard.kt
@@ -22,4 +22,14 @@ fun main() {
     println("Closed Trades: ${closedTrades.size}")
     println("Winning Trades Count: ${winningTrades.size}")
     println("Losing Trades Count: ${losingTrades.size}")
+
+    // Pipeline 4: Analisis Koin Profit Tertinggi (Sorting & Mapping)
+    val topPerformersString = winningTrades
+        .sortedByDescending { it.roe } // Urutkan dari profit tertinggi
+        .map {
+            "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
+
+    println("\n=== TOP PERFORMERS ===")
+    topPerformersString.forEach { println(it) }
 }

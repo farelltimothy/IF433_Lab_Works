e3c152379f0acd851be5b9e1cc87998c23a26d5f
farelltimo <farelltimothy@gmail.com>
2026-05-08 09:38:30 +0700

    week08: implement smart cast for email notification

---
+++ src/OOP_108839_FarellTimothy/week08/NotificationService.kt
@@ -0,0 +1,18 @@
+package OOP_108839_FarellTimothy.week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        // sendEmail(user.email) // INI AKAN ERROR: Type mismatch karena email bisa null
+
+        if (user.email != null) {
+            // SUCCESS via Smart Cast: compiler tahu 'user.email' pasti tidak null di blok ini
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}

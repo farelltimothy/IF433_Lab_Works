ce0a5f47071864bd03453222ece3768bf4f5db97
farelltimo <farelltimothy@gmail.com>
2026-05-08 10:41:55 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/OOP_108839_FarellTimothy/week08/EcomerceModels.kt
@@ -0,0 +1,15 @@
+package OOP_108839_FarellTimothy.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()

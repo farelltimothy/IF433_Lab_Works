d1fa007b666ba230f4a01990fcc069210ce6c348
farelltimo <farelltimothy@gmail.com>
2026-05-08 12:32:06 +0700

    week11: (task) configure Ezviz camera with also

--- src/OOP_108839_FarellTimothy/week11/SmartHomeDashboard.kt
+++ src/OOP_108839_FarellTimothy/week11/SmartHomeDashboard.kt
@@ -3,10 +3,20 @@ package oop_108839_FarellTimothy.week11
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
+    // Perangkat 1: Pencahayaan
     SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
     }.also {
+        homeDevices.add(it)
+    }
+
+    // Perangkat 2: Keamanan (Checkpoint 15)
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
 }

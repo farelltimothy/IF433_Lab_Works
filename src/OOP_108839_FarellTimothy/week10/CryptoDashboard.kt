1e704682b73788be4a7670678d2ac01037c9936b
farelltimo <farelltimothy@gmail.com>
2026-05-08 12:08:23 +0700

    week10: (task) populate coin repository

--- src/OOP_108839_FarellTimothy/week10/CryptoDashboard.kt
+++ src/OOP_108839_FarellTimothy/week10/CryptoDashboard.kt
@@ -3,22 +3,29 @@ package oop_108839_FarellTimothy.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
 
-    val btc = Coin("Bitcoin", 0.5)
-    val eth = Coin("Ethereum", 10.0)
+    val btc = Coin("BTC", 0.5)
+    val eth = Coin("ETH", 10.0)
+    val usdt = Coin("USDT", 5000.0)
 
     coinRepo.add(btc)
     coinRepo.add(eth)
+    coinRepo.add(usdt)
 
     val allCoins = coinRepo.getAll()
     val response = ApiResponse("Success", allCoins)
 
     println("=== CRYPTO WALLET DASHBOARD ===")
     println("Status: ${response.status}")
-    println("Daftar Koin:")
+    println("Daftar Aset Dompet:")
     response.data.forEach {
-        println("- ${it.name}: ${it.balance} BTC/ETH")
+        println("- ${it.name}: ${it.balance}")
     }
 
-    val searchResult = coinRepo.searchByName("Bitcoin")
-    println("\nHasil Pencarian 'Bitcoin': $searchResult")
+    println("\n=== MENCARI ASET ===")
+    val searchResult = coinRepo.searchByName("USDT")
+    if (searchResult != null) {
+        println("Aset ditemukan: $searchResult")
+    } else {
+        println("Aset tidak ditemukan.")
+    }
 }

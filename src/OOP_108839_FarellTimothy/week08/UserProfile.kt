39915b19f25c4db6c26be38c9dc75efe1a570ee6
farelltimo <farelltimothy@gmail.com>
2026-05-08 09:36:37 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/OOP_108839_FarellTimothy/week08/UserProfile.kt
@@ -0,0 +1,8 @@
+package OOP_108839_FarellTimothy.week08
+
+// name adalah Non-Null (wajib), email dan phone adalah Nullable (opsional)
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null // Default argument null
+)

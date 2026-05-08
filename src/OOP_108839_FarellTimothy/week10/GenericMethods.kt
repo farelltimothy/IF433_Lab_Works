407ffe3ef62119a0af605bffa7f1a4d72faaeac3
farelltimo <farelltimothy@gmail.com>
2026-05-08 11:56:21 +0700

    week10: implement generic function with return type

--- src/OOP_108839_FarellTimothy/week10/GenericMethods.kt
+++ src/OOP_108839_FarellTimothy/week10/GenericMethods.kt
@@ -1,14 +1,25 @@
 package oop_108839_FarellTimothy.week10
 
-// Definisi fungsi generik dengan awalan <T> sebelum nama fungsi
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
 
+fun <T> processData(input: T): T {
+    return input
+}
+
 fun main() {
-    // Menguji fungsi dengan berbagai tipe data
-    printData(12345)                 // Tipe Int
-    printData("Belajar Kotlin")      // Tipe String
-    printData(98.5)                  // Tipe Double
-    printData(true)                  // Tipe Boolean
+    println("=== TESTING GENERIC METHODS ===")
+
+    printData(2024)
+    printData("Halo Dunia")
+    printData(3.14)
+
+    println("\n--- Testing Return Type ---")
+
+    val hasilInt = processData(500)
+    val hasilString = processData("Kotlin Generics")
+
+    println("Hasil Process Int: $hasilInt")
+    println("Hasil Process String: $hasilString")
 }

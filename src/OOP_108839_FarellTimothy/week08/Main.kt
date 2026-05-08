dad3ddc853f7c6c3d39479861f773d2eef2019fc
farelltimo <farelltimothy@gmail.com>
2026-05-08 09:50:50 +0700

    week08: create mixed data collection

--- src/OOP_108839_FarellTimothy/week08/Main.kt
+++ src/OOP_108839_FarellTimothy/week08/Main.kt
@@ -4,7 +4,6 @@ fun main() {
     println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
 
-    // Rantai Safe Calls yang elegan
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
 
     println("Tujuan pengiriman: $destination")
@@ -13,11 +12,19 @@ fun main() {
     val validOrder = Order(null, 250000)
 
     val receipt = validOrder.totalPrice?.let { price ->
-        // Blok ini HANYA jalan jika totalPrice tidak null
         val tax = price * 0.11
         "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }
 

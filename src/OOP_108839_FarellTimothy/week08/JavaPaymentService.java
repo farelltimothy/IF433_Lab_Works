7bc44b788f67052903890860f9451e75232d7f58
farelltimo <farelltimothy@gmail.com>
2026-05-08 10:46:35 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/OOP_108839_FarellTimothy/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package OOP_108839_FarellTimothy.week08;
+
+public class JavaPaymentService {
+    // Method static untuk mensimulasikan proses pembayaran lama
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}

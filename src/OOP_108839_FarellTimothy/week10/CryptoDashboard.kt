d5dd8d7f82060b4f7c5828e728385a5c68c6cb31
farelltimo <farelltimothy@gmail.com>
2026-05-08 12:11:13 +0700

    week10: (task) initialize and populate transaction repository

--- src/OOP_108839_FarellTimothy/week10/CryptoDashboard.kt
+++ src/OOP_108839_FarellTimothy/week10/CryptoDashboard.kt
@@ -2,7 +2,6 @@ package oop_108839_FarellTimothy.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
-
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 5000.0))
@@ -12,8 +11,20 @@ fun main() {
     println("=== CRYPTO WALLET DASHBOARD ===")
     println("Status: ${response.status}")
     println("Daftar Aset:")
-
     response.data.forEach { coin ->
         println("Nama Koin: ${coin.name}, Saldo: ${coin.balance}")
+    }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX1001", 0.05))
+    txRepo.add(Transaction("TX1002", 1.2))
+    txRepo.add(Transaction("TX1003", 500.0))
+
+    val txResponse = ApiResponse("200 OK", txRepo.getAll())
+
+    println("\n=== TRANSACTION HISTORY ===")
+    println("Status: ${txResponse.status}")
+    txResponse.data.forEach { tx ->
+        println("ID Transaksi: ${tx.id}, Jumlah: ${tx.amount}")
     }
 }

8c9ceae1459f2225dcac9ee2eed57a5e4857402e
farelltimo <farelltimothy@gmail.com>
2026-05-08 12:36:41 +0700

    week11: (task) implement device search with let

--- src/OOP_108839_FarellTimothy/week11/SmartHomeDashboard.kt
+++ src/OOP_108839_FarellTimothy/week11/SmartHomeDashboard.kt
@@ -3,7 +3,6 @@ package oop_108839_FarellTimothy.week11
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
-    // Perangkat 1: Pencahayaan
     SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
@@ -11,7 +10,6 @@ fun main() {
         homeDevices.add(it)
     }
 
-    // Perangkat 2: Keamanan
     SmartDevice("Ezviz Outdoor", "Camera").apply {
         isOnline = true
         powerLoad = 5
@@ -20,12 +18,19 @@ fun main() {
         homeDevices.add(it)
     }
 
-    // Perangkat 3 & 4: AC & Pet Care (Checkpoint 16)
     val acInverter = run {
         val device = SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
-        device // Menjadi return value untuk acInverter
+        device
     }
     homeDevices.add(acInverter)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    println("\n=== SEARCH DEVICE ===")
+    val searchResult = homeDevices.find { it.category == "Camera" }
+
+    searchResult?.let {
+        println("Perangkat Ditemukan:")
+        println(it.diagnose())
+    }
 }

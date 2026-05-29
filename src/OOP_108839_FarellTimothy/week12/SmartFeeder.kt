4df11198f6b9913b0b8956f1a82d33cce3470ab8
unknown <abidichadd@gmail.com>
2026-05-29 11:09:04 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/OOP_108839_FarellTimothy/week12/SmartFeeder.kt
@@ -0,0 +1,14 @@
+package OOP_108839_FarellTimothy.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    return availableGram - requestedGram
+}

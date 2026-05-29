ccdd17b1f6e9f0865d2970e40a244e98beb2e342
unknown <abidichadd@gmail.com>
2026-05-29 11:15:02 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/OOP_108839_FarellTimothy/week12/SmartFeeder.kt
+++ src/OOP_108839_FarellTimothy/week12/SmartFeeder.kt
@@ -16,4 +16,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Gagal: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Gagal: ${e.message}")
+    } catch (e: Exception) {
+        println("Terjadi kesalahan umum: ${e.message}")
+    }
 }

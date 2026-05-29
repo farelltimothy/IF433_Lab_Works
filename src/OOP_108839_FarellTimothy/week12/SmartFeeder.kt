8d148c26f20b24eb128f6223b6fba6dac0318522
unknown <abidichadd@gmail.com>
2026-05-29 11:12:33 +0700

    week12: (task) implement food stock validation with throw

--- src/OOP_108839_FarellTimothy/week12/SmartFeeder.kt
+++ src/OOP_108839_FarellTimothy/week12/SmartFeeder.kt
@@ -4,11 +4,12 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
     if (isJammed) {
-        throw DispenserJamException() //sudah true
+        throw DispenserJamException()
     }
     if (requestedGram > availableGram) {
         throw FoodEmptyException(requestedGram, availableGram)
     }
 
+    println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
